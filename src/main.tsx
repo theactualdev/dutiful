c5a568d68f5046d0be831a43d03633b6df8d0e1c
theactualdev <olayinkacodes@gmail.com>
2024-10-24 03:51:10 +0100

    fix: fixes default router

--- src/main.tsx
+++ src/main.tsx
@@ -55,6 +55,10 @@ const router = createBrowserRouter([
         path: "/passreset",
         element: <Reset />
       },
+      {
+        path: "/",
+        element: <Home />
+      },
     ] 
   }
 ])
